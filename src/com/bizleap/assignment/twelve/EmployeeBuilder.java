53666271adf7272145d688cfad9c613ba527c14b
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-03-07 12:12:58 +0630

    5 & 12

---
+++ src/com/bizleap/assignment/twelve/EmployeeBuilder.java
@@ -0,0 +1,31 @@
+package com.bizleap.assignment.twelve;
+
+/**
+ * Created by M2h_Laptop on 06-Mar-18.
+ */
+public class EmployeeBuilder {
+    public static void main(String[] args) {
+
+    }
+}
+class Employee {
+    String name,age,dob;
+    public void setName(String n){
+        name = n;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setAge(String a){
+        age = a;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setDob(String dob){
+        dob = dob;
+    }
+    public String getDob(){
+        return dob;
+    }
+}
