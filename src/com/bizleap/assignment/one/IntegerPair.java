c825200aba1775ead56f6d57601772d9c0b7d309
BizLeapTechnology <bizleaptechnologyassignment@gmail.com>
2018-03-07 12:04:27 +0630

    Update

--- src/com/bizleap/assignment/one/IntegerPair.java
+++ src/com/bizleap/assignment/one/IntegerPair.java
@@ -1,29 +1,24 @@
 package com.bizleap.assignment.one;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by M2h_Laptop on 1/15/2018.
  */
 public class IntegerPair {
     public static void main(String args[]) {
-        int i = 1, j = 2;
-        for (int n = 1; n < 21; n++)
-
-            if (n % 2 == 1) {
-                System.out.println(n + " = " + (n * i));
-                i = i + 1;
-            } else {
-                System.out.println(n + " = " + ((n * j) - (n / 2)));
-                j = j + 1;
-            }
-
-        System.out.println("------------------------------------------------------------------------------");
-        System.out.println("------------------------------------------------------------------------------");
-        System.out.println("------------------------------------------------------------------------------");
+        IntegerPair integerPair = new IntegerPair();
+        System.out.println(integerPair.getIntegerPairMap());
+    }
+    public Map getIntegerPairMap(){
+        Map<Integer,Integer> integerMap = new <Integer,Integer>HashMap();
         int temp = 0;
-        for (int k = 1; k < 21; k++) {
+        for (int i = 1; i < 21; i++) {
 
-            temp = temp + k;
-            System.out.println(k + "=" + temp);
+            temp = temp + i;
+            integerMap.put(i,temp);
         }
+        return integerMap;
     }
 }
